0c88aa0251ed3c713b4cdcce84e3f3454dc0dc94
ateplyakov <antondmtvch@gmail.com>
2020-09-19 18:11:36 +0300

    Added option --host

--- scoring_api/api/api.py
+++ scoring_api/api/api.py
@@ -298,10 +298,11 @@ def main():
     op = OptionParser()
     op.add_option("-p", "--port", action="store", type=int, default=8080)
     op.add_option("-l", "--log", action="store", default=None)
+    op.add_option("--host", action="store", type=str, default="localhost")
     opts, args = op.parse_args()
     logging.basicConfig(filename=opts.log, level=logging.INFO,
                         format='[%(asctime)s] %(levelname).1s %(message)s', datefmt='%Y.%m.%d %H:%M:%S')
-    server = HTTPServer(("localhost", opts.port), MainHTTPHandler)
+    server = HTTPServer((opts.host, opts.port), MainHTTPHandler)
     logging.info("Starting server at %s" % opts.port)
     try:
         server.serve_forever()
