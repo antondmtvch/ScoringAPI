07417aa6a6f5487677a374be254f2993f0e0eb4f
ateplyakov <antondmtvch@gmail.com>
2020-09-26 17:11:56 +0300

    All validate decorators updated

--- scoring_api/api/validators.py
+++ scoring_api/api/validators.py
@@ -6,24 +6,21 @@ from scoring_api.api.exceptions import ValidationError
 __all__ = ['type_validator', 'email_validator', 'phone_validator', 'date_validator']
 
 
-def type_validator(*types):
-    def validator(func):
-        @wraps(func)
-        def wrapper(self, value):
-            if not isinstance(value, types):
-                t = ' or '.join((t.__name__ for t in types))
-                raise ValidationError(f'{repr(self.name)} must be {t}, not {value.__class__.__name__}')
-            return func(self, value)
-        return wrapper
-    return validator
+def type_validator(func):
+    @wraps(func)
+    def wrapper(self, value):
+        if not isinstance(value, self.types):
+            t = ' or '.join((t.__name__ for t in self.types))
+            raise ValidationError(f'{repr(self.name)} must be {t}, not {value.__class__.__name__}')
+        return func(self, value)
+    return wrapper
 
 
 def email_validator(func):
     pattern = re.compile(r'^[._\w]+@\w+\.\w{2,10}$')
     @wraps(func)
-    @type_validator(str)
     def wrapper(self, value):
-        if not re.match(pattern, str(value)):
+        if not re.match(pattern, value):
             raise ValidationError(f'{value} is not valid email')
         return func(self, value)
     return wrapper
@@ -32,7 +29,6 @@ def email_validator(func):
 def phone_validator(func):
     pattern = re.compile(r'^7\d{10}$')
     @wraps(func)
-    @type_validator(str, int)
     def wrapper(self, value):
         if not re.match(pattern, str(value)):
             raise ValidationError(f'{value} is not valid phone number')
@@ -43,10 +39,9 @@ def phone_validator(func):
 def date_validator(fmt):
     def validator(func):
         @wraps(func)
-        @type_validator(str)
         def wrapper(self, value):
             try:
-                datetime.strptime(value, fmt)
+                self.parsed_date = datetime.strptime(value, fmt)
             except ValueError:
                 raise ValidationError(f'invalid date format for {repr(self.name)} field, expected {fmt}')
             return func(self, value)
